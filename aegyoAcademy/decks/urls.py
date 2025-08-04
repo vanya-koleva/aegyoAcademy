24bf4f4e6cae7e16666e6f62130567f7cfb73f53
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-08-04 23:19:34 +0300

    chore: edit the deck urls

--- aegyoAcademy/decks/urls.py
+++ aegyoAcademy/decks/urls.py
@@ -1,21 +1,22 @@
-from django.urls import path, include
+from django.urls import path
 
 from decks import views
 
 urlpatterns = [
     path('dashboard/', views.Dashboard.as_view(), name='dashboard'),
-    path('', include([
-        path('<int:pk>/', views.DeckDetailView.as_view(), name='deck-detail'),
-        path('<int:pk>/edit/', views.DeckUpdateView.as_view(), name='deck-edit'),
-        path('<int:pk>/delete/', views.DeckDeleteView.as_view(), name='deck-delete'),
-        path('<int:deck_id>/flashcards/', views.FlashcardListView.as_view(), name='flashcards-list'),
-        path('<int:deck_id>/add-flashcard/', views.FlashcardCreateView.as_view(), name='flashcard-add'),
-        path('<int:deck_id>/flashcards/<int:pk>/edit/', views.FlashcardUpdateView.as_view(), name='flashcard-edit'),
-        path('<int:deck_id>/flashcards/<int:pk>/delete/', views.FlashcardDeleteView.as_view(), name='flashcard-delete'),
-        path('create/', views.DeckCreateView.as_view(), name='deck-create'),
-        path('<int:deck_id>/study/', views.StudySessionStartView.as_view(), name='study-session-start'),
-        path('<int:deck_id>/study/card/', views.StudySessionCardView.as_view(), name='study-session-card'),
-        path('<int:deck_id>/study/answer/', views.StudySessionAnswerView.as_view(), name='study-session-answer'),
-        path('study/<int:session_id>/results/', views.StudySessionResultsView.as_view(), name='study-session-results'),
-    ]))
+
+    path('create/', views.DeckCreateView.as_view(), name='deck-create'),
+    path('<int:pk>/', views.DeckDetailView.as_view(), name='deck-detail'),
+    path('<int:pk>/edit/', views.DeckUpdateView.as_view(), name='deck-edit'),
+    path('<int:pk>/delete/', views.DeckDeleteView.as_view(), name='deck-delete'),
+
+    path('<int:deck_id>/flashcards/', views.FlashcardListView.as_view(), name='flashcards-list'),
+    path('<int:deck_id>/add-flashcard/', views.FlashcardCreateView.as_view(), name='flashcard-add'),
+    path('<int:deck_id>/flashcards/<int:pk>/edit/', views.FlashcardUpdateView.as_view(), name='flashcard-edit'),
+    path('<int:deck_id>/flashcards/<int:pk>/delete/', views.FlashcardDeleteView.as_view(), name='flashcard-delete'),
+
+    path('<int:deck_id>/study/', views.StudySessionStartView.as_view(), name='study-session-start'),
+    path('<int:deck_id>/study/card/', views.StudySessionCardView.as_view(), name='study-session-card'),
+    path('<int:deck_id>/study/answer/', views.StudySessionAnswerView.as_view(), name='study-session-answer'),
+    path('study/<int:session_id>/results/', views.StudySessionResultsView.as_view(), name='study-session-results'),
 ]
